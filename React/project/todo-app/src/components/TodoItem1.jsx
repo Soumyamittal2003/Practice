93c7579a75942768e223c4c0d6c98cb821830d03
Soumya Mittal <soumyamittalrgh@gmail.com>
2024-07-02 22:43:17 +0530

    react project

---
+++ React/project/todo-app/src/components/TodoItem1.jsx
@@ -0,0 +1,22 @@
+function TodoItem1() {
+    
+  return (
+    <div class="container text-center">
+      <div className="row">
+        <div class="col-6">Buy milk</div>
+        <div class="col-4">4/10/2023</div>
+        
+
+      <div class="col-2">
+        <button type="button" class="btn btn-danger">
+          Delete
+        </button>
+        </div>
+      </div>
+    </div>
+
+
+  );
+}
+
+export default TodoItem1;
